05f55eaf8c8d560171f6e3ab616141e35457b1d7
Zebreus <lennarteichhorn@googlemail.com>
2022-10-10 15:57:59 +0200

    Fix url function

--- src/functions/getOwnUrl.ts
+++ src/functions/getOwnUrl.ts
@@ -1,7 +1,9 @@
 export const getOwnUrl = () => {
   const vercelUrl = process.env["VERCEL_URL"] as string | undefined
+  const publicVercelUrl = process.env["NEXT_PUBLIC_VERCEL_URL"] as string | undefined
   const gitpodWorkspaceUrl = process.env["GITPOD_WORKSPACE_URL"] as string | undefined
   const gitpodUrl = gitpodWorkspaceUrl?.replace(/^https:\/\//, "https://3000-")
 
-  return vercelUrl || gitpodUrl
+  const url = vercelUrl || publicVercelUrl || gitpodUrl || "generated.fashion"
+  return url.startsWith("http") ? url : "https://" + url
 }
