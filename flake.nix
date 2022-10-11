6652796aadd245e08849ea56c504bd5f7859a2da
Zebreus <lennarteichhorn@googlemail.com>
2022-10-11 14:14:56 +0200

    Fix nix flake

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
             export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
             export PRISMA_INTROSPECTION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/introspection-engine"
             export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
-          '');yarn
+          '');
         };
 
     };
