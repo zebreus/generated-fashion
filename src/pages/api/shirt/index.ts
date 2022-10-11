1c5c5f6f4c1764b81a49835353d5b980196c0435
Zebreus <lennarteichhorn@googlemail.com>
2022-10-11 17:56:52 +0200

    Fix edge function

--- src/pages/api/shirt/index.ts
+++ src/pages/api/shirt/index.ts
@@ -3,14 +3,14 @@ import { setDoc } from "firebase/firestore"
 import { generateId } from "functions/generateId"
 import { getOwnUrl } from "functions/getOwnUrl"
 import { getPredictionRef } from "hooks/firestore/getRefs"
-import { NextApiHandler } from "next"
+import { NextRequest } from "next/server"
 import { predict } from "replicate-api"
 
 const replicateVersion = "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef"
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introductio
-const handler: NextApiHandler = async req => {
-  const { prompt } = req.body
+const handler = async (req: NextRequest) => {
+  const { prompt } = await req.json()
   if (!is.string(prompt)) {
     return new Response(JSON.stringify({ detail: "a string prompt is required" }), {
       status: 400,
@@ -39,6 +39,29 @@ const handler: NextApiHandler = async req => {
   const id = generateId()
   const seed = Math.floor(Math.random() * 100000000)
 
+  // const ref = getPredictionRef(id)
+  // await setDoc(ref, {
+  //   createdAt: Date.now(),
+  //   prompt: prompt,
+  //   seed: seed,
+  //   state: "starting",
+  //   version: replicateVersion,
+  //   _ref: ref,
+  // })
+
+  const backgroundPromise = backgroundHandler(prompt, id, seed)
+
+  await backgroundPromise
+
+  return new Response(JSON.stringify({ id: id }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+}
+
+const backgroundHandler = async (prompt: string, id: string, seed: number) => {
   const prediction = await predict({
     version: replicateVersion,
     input: { prompt: prompt, steps: 10, seed: seed },
@@ -51,19 +74,17 @@ const handler: NextApiHandler = async req => {
   await setDoc(newRef, {
     createdAt: Date.now(),
     prompt: prompt,
-    seed: seed,
     replicateId: prediction.id,
-    state: prediction.status,
-    version: prediction.version,
+    seed: seed,
+    state: "starting",
+    version: replicateVersion,
     _ref: newRef,
   })
 
-  return new Response(JSON.stringify({ id: id }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  // await updateDoc(newRef, {
+  //   replicateId: prediction.id,
+  //   state: prediction.status,
+  // })
 }
 
 export default handler
--- src/types/firestore/prediction.ts
+++ src/types/firestore/prediction.ts
@@ -3,7 +3,7 @@ export type PredictionState = "starting" | "processing" | "succeeded" | "failed"
 export type Prediction = {
   replicateId?: string
   version: string
-  state: string
+  state: PredictionState
   prompt: string
   seed: number
   resultUrl?: string
