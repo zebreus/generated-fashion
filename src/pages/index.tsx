9df79010aff8730588ec82ca5df0f852717e248f
Zebreus <lennarteichhorn@googlemail.com>
2022-10-14 13:59:16 +0200

    Remove PatternBackground

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import { Gallery } from "components/Gallery"
 import { Generator } from "components/Generator"
-import { PatternBackground } from "components/PatternBackground"
 
 export default function ShirtPreview() {
   return (
-    <PatternBackground light>
-      <main className="flex flex-col min-h-screen items-center justify-between">
-        <h2 className="sm:text-5xl text-3xl m-3">generated.fashion</h2>
-
-        <Generator />
-        <Gallery />
-      </main>
-    </PatternBackground>
+    <main className="flex flex-col min-h-screen items-center justify-between">
+      <h2 className="sm:text-5xl text-3xl m-3">generated.fashion</h2>
+      <Generator />
+      <Gallery />
+    </main>
   )
 }
