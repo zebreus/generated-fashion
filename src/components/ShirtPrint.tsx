b7263979ebc786a74a14821475e0b4da16a93297
Zebreus <lennarteichhorn@googlemail.com>
2022-10-24 22:41:35 +0200

    Add page for generating the print design

---
+++ src/components/ShirtPrint.tsx
@@ -0,0 +1,15 @@
+type ShirtPrintProps = {
+  imageUrl: string
+  prompt: string
+  qrCodeUrl: string
+  qrCodeText: string
+}
+
+export const ShirtPrint = ({ imageUrl, prompt, qrCodeUrl, qrCodeText }: ShirtPrintProps) => {
+  imageUrl
+  prompt
+  qrCodeUrl
+  qrCodeText
+  //   const element = generatedQrElement()
+  return <div style={{ width: "100%", height: "100%", background: "green", display: "flex" }}></div>
+}
---
+++ src/pages/shirt/[id]/print.tsx
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react"
+import { ShirtPrint } from "components/ShirtPrint"
+import { usePrediction } from "hooks/firestore/simple/usePrediction"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+const dpi = 150
+
+const ShirtImage = () => {
+  const router = useRouter()
+  const id = typeof router.query["id"] === "string" ? router.query["id"] : router.query["id"]?.[0]
+
+  const prediction = usePrediction(id)
+  const url = prediction?.resultUrl
+
+  const [height, setHeight] = useState(0)
+  useEffect(() => {
+    // document.documentElement.clientHeight
+    setHeight(window.innerHeight)
+  }, [setHeight])
+
+  return (
+    <div
+      css={css`
+        position: fixed;
+        height: ${16 * dpi}px;
+        width: ${12 * dpi}px;
+        background: black;
+        transform-origin: top center;
+        transform: scale(${height / (16 * dpi)});
+      `}
+    >
+      <ShirtPrint
+        prompt={prediction?.prompt || "Test prompt"}
+        imageUrl={url || "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"}
+        qrCodeText={"funny qr code"}
+        qrCodeUrl={"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"}
+      />
+    </div>
+  )
+}
+
+export default ShirtImage
