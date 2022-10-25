b4c58afe229873b77b0a29bccce1ccc51fffdfc1
Zebreus <lennarteichhorn@googlemail.com>
2022-10-25 16:06:46 +0200

    Always enable documenttransitions when available

--- src/hooks/useLinkClickHandler.ts
+++ src/hooks/useLinkClickHandler.ts
@@ -1,5 +1,5 @@
 import { NextRouter, useRouter } from "next/router"
-import { SyntheticEvent, useMemo } from "react"
+import { useMemo } from "react"
 
 export const useLinkClickHandler = (url?: string) => {
   const router = useRouter()
@@ -14,22 +14,25 @@ export const useLinkClickHandler = (url?: string) => {
 }
 
 export const createLinkClickHandler = (url: string, router: NextRouter) => {
-  if (
-    typeof window === "undefined" ||
-    !(document as { createDocumentTransition?: (cb: () => void) => void }).createDocumentTransition
-  ) {
-    console.error("Document transitions are unavailable")
-    return () => {}
-  }
-  return (e: SyntheticEvent) => {
-    e.preventDefault()
-    const transition = (
-      document as unknown as {
-        createDocumentTransition: () => { start: (cb: () => Promise<void> | void) => void }
-      }
-    ).createDocumentTransition()
-    transition.start(async () => {
-      await router.push(url)
-    })
-  }
+  url
+  router
+  return () => undefined
+  // if (
+  //   typeof window === "undefined" ||
+  //   !(document as { createDocumentTransition?: (cb: () => void) => void }).createDocumentTransition
+  // ) {
+  //   console.error("Document transitions are unavailable")
+  //   return () => {}
+  // }
+  // return (e: SyntheticEvent) => {
+  //   e.preventDefault()
+  //   const transition = (
+  //     document as unknown as {
+  //       createDocumentTransition: () => { start: (cb: () => Promise<void> | void) => void }
+  //     }
+  //   ).createDocumentTransition()
+  //   transition.start(async () => {
+  //     await router.push(url)
+  //   })
+  // }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { CommonHead } from "components/CommonHead"
 import { useInitialize } from "hooks/useInitialize"
 import { NextPage } from "next"
 import { AppProps } from "next/dist/shared/lib/router/router"
-import { ReactElement, ReactNode } from "react"
+// eslint-disable-next-line import/no-named-as-default
+import Router from "next/router"
+import { ReactElement, ReactNode, useEffect } from "react"
 // eslint-disable-next-line import/no-unassigned-import
 import "styles/globals.css"
 
@@ -19,57 +21,61 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   const getLayout = Component.getLayout ?? (page => page)
 
-  // useEffect(() => {
-  //   let currentUrl: string | undefined
-  //   let currentCallback: (() => void) | undefined
-  //   const startHandler = (url: string, { shallow }: { shallow?: boolean }) => {
-  //     if (!shallow && !currentCallback) {
-  //       if (!(document as { createDocumentTransition?: (cb: () => void) => void }).createDocumentTransition) {
-  //         console.error("Document transitions are unavailable")
-  //         return
-  //       }
-  //       const transition = (
-  //         document as unknown as {
-  //           createDocumentTransition: () => { start: (cb: () => Promise<void> | void) => void }
-  //         }
-  //       ).createDocumentTransition()
-  //       const myPromise = new Promise<void>(resolve => {
-  //         currentUrl = url
-  //         currentCallback = () => {
-  //           resolve()
-  //         }
-  //       })
-  //       transition.start(async () => {
-  //         await myPromise
-  //       })
-  //     }
-  //     // routeChangePromise = new Promise(resolve => {
-  //     //   Router.events.once("routeChangeComplete", resolve)
-  //     // })
+  useEffect(() => {
+    if (!(document as { createDocumentTransition?: (cb: () => void) => void }).createDocumentTransition) {
+      console.error("Document transitions are unavailable")
+      return
+    }
+    let currentUrl: string | undefined
+    let currentCallback: (() => void) | undefined
+    const startHandler = (url: string, { shallow }: { shallow?: boolean }) => {
+      if (!shallow && !currentCallback) {
+        if (!(document as { createDocumentTransition?: (cb: () => void) => void }).createDocumentTransition) {
+          console.error("Document transitions are unavailable")
+          return
+        }
+        const transition = (
+          document as unknown as {
+            createDocumentTransition: () => { start: (cb: () => Promise<void> | void) => void }
+          }
+        ).createDocumentTransition()
+        const myPromise = new Promise<void>(resolve => {
+          currentUrl = url
+          currentCallback = () => {
+            resolve()
+          }
+        })
+        transition.start(async () => {
+          await myPromise
+        })
+      }
+      // routeChangePromise = new Promise(resolve => {
+      //   Router.events.once("routeChangeComplete", resolve)
+      // })
 
-  //     console.log("start", url)
-  //   }
-  //   const completeHandler = (url: string) => {
-  //     if (currentUrl === url && currentCallback) {
-  //       currentCallback()
-  //       currentCallback = undefined
-  //       currentUrl = undefined
-  //     }
+      console.log("start", url)
+    }
+    const completeHandler = (url: string) => {
+      if (currentUrl === url && currentCallback) {
+        currentCallback()
+        currentCallback = undefined
+        currentUrl = undefined
+      }
 
-  //     console.log("complete", url)
-  //   }
-  //   const errorHandler = (...evts: unknown[]) => {
-  //     console.log("error", evts)
-  //   }
-  //   Router.events.on("routeChangeStart", startHandler)
-  //   Router.events.on("routeChangeComplete", completeHandler)
-  //   Router.events.on("routeChangeError", errorHandler)
-  //   return () => {
-  //     Router.events.off("routeChangeStart", startHandler)
-  //     Router.events.off("routeChangeComplete", completeHandler)
-  //     Router.events.off("routeChangeError", errorHandler)
-  //   }
-  // }, [])
+      console.log("complete", url)
+    }
+    const errorHandler = (...evts: unknown[]) => {
+      console.log("error", evts)
+    }
+    Router.events.on("routeChangeStart", startHandler)
+    Router.events.on("routeChangeComplete", completeHandler)
+    Router.events.on("routeChangeError", errorHandler)
+    return () => {
+      Router.events.off("routeChangeStart", startHandler)
+      Router.events.off("routeChangeComplete", completeHandler)
+      Router.events.off("routeChangeError", errorHandler)
+    }
+  }, [])
 
   return (
     <>
